529c6befb4f0b6d1dc999328cc307aa2b54f732e
Steve_Wang21 <60515236+wangzihaoyt36@users.noreply.github.com>
2020-12-28 23:47:21 +0800

    Update tf_grouping_compile.sh

--- tf_ops/grouping/tf_grouping_compile.sh
+++ tf_ops/grouping/tf_grouping_compile.sh
@@ -19,8 +19,8 @@
 #/bin/bash
 /usr/local/cuda-10.1/bin/nvcc tf_grouping_g.cu -o tf_grouping_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
-TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+TF_INC=$(shell python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(shell python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 CUDA_INC='/usr/local/cuda-10.1/include'
 CUDA_LIB='/usr/local/cuda-10.1/lib64/'
 
