4bbe09c7275b1e453098a33dd831c9a0baf26022
Steve_Wang21 <60515236+wangzihaoyt36@users.noreply.github.com>
2020-12-28 23:47:42 +0800

    Update tf_sampling_compile.sh

--- tf_ops/sampling/tf_sampling_compile.sh
+++ tf_ops/sampling/tf_sampling_compile.sh
@@ -18,8 +18,8 @@
 #/bin/bash
 /usr/local/cuda-10.1/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
-TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+TF_INC=$(shell python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(shell python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 CUDA_INC='/usr/local/cuda-10.1/include'
 CUDA_LIB='/usr/local/cuda-10.1/lib64/'
 
