f69dd349471d94f5df5d2624e8111438e3568cd7
Steve_Wang21 <60515236+wangzihaoyt36@users.noreply.github.com>
2020-12-29 00:38:47 +0800

    Update tf_interpolate_compile.sh

--- tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -8,14 +8,7 @@
 #g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /home/bai/anaconda3/envs/tfgpu1-13/lib/python3.7/site-packages/tensorflow/include/ -I /usr/local/cuda-10.1/include -I /home/bai/anaconda3/envs/tfgpu1-13/lib/python3.7/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-10.1/lib64/ -L/home/bai/anaconda3/envs/tfgpu1-13/lib/python3.7/site-packages/tensorflow/  -ltensorflow_framework -O2 #-D_GLIBCXX_USE_CXX11_ABI=0 
 
 
-#TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-#TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
-
-#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I$TF_INC -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-10.0/lib64/ -L$TF_LIB  -ltensorflow_framework -O2 #-D_GLIBCXX_USE_CXX11_ABI=0 
-
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-TF_LIB=$(shell python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
-CUDA_INC='/usr/local/cuda-10.1/include'
-CUDA_LIB='/usr/local/cuda-10.1/lib64/'
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -I $TF_INC -I $CUDA_INC -lcudart -L $CUDA_LIB -L $TF_LIB -ltensorflow_framework -shared -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I$TF_INC -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-10.1/lib64/ -L$TF_LIB  -libtensorflow_framework -O2 #-D_GLIBCXX_USE_CXX11_ABI=0 
