fb9b9914e9cfa70f1f6afd1f3f418ac7333ffa0d
Steve_Wang21 <60515236+wangzihaoyt36@users.noreply.github.com>
2020-12-29 00:36:35 +0800

    Update tf_sampling_compile.sh

--- tf_ops/sampling/tf_sampling_compile.sh
+++ tf_ops/sampling/tf_sampling_compile.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-#/usr/local/cuda-10.0/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+/usr/local/cuda-10.1/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 # TF1.2
 #g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
@@ -10,17 +10,7 @@
 # TF1.13
 #g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I /home/bai/anaconda3/envs/tfgpu1.13/lib/python3.7/site-packages/tensorflow/include/ -I /usr/local/cuda-10.1/include -I /home/bai/anaconda3/envs/tfgpu1.13/lib/python3.7/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-10.1/lib64/ -L/home/bai/anaconda3/envs/tfgpu1.13/lib/python3.7/site-packages/tensorflow/ -ltensorflow_framework -O2 #-D_GLIBCXX_USE_CXX11_ABI=0  
 
-#TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-#TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
-
-#g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I$TF_INC -I /usr/local/cuda-10.0/include -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-10.0/lib64/ -L$TF_LIB -ltensorflow_framework -O2 #-D_GLIBCXX_USE_CXX11_ABI=0  
-
-#/bin/bash
-/usr/local/cuda-10.1/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-TF_LIB=$(shell python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
-CUDA_INC='/usr/local/cuda-10.1/include'
-CUDA_LIB='/usr/local/cuda-10.1/lib64/'
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
-g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I $TF_INC -I $CUDA_INC -I $TF_INC/external/nsync/public -lcudart -L $CUDA_LIB -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I$TF_INC -I /usr/local/cuda-10.1/include -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-10.1/lib64/ -L$TF_LIB -libtensorflow_framework -O2 #-D_GLIBCXX_USE_CXX11_ABI=0  
