f3d7f96c7bc0830330ab3a119dbae3b7b66907ad
Steve_Wang21 <60515236+wangzihaoyt36@users.noreply.github.com>
2020-12-28 23:52:56 +0800

    Update tf_interpolate_compile.sh

--- tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -13,7 +13,7 @@
 
 #g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I$TF_INC -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-10.0/lib64/ -L$TF_LIB  -ltensorflow_framework -O2 #-D_GLIBCXX_USE_CXX11_ABI=0 
 
-TF_INC=$(shell python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(shell python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 CUDA_INC='/usr/local/cuda-10.1/include'
 CUDA_LIB='/usr/local/cuda-10.1/lib64/'
