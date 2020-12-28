cd26169aac51543a991ddb28b175de396867e35e
Steve_Wang21 <60515236+wangzihaoyt36@users.noreply.github.com>
2020-12-28 23:39:09 +0800

    Update tf_grouping_compile.sh

--- tf_ops/grouping/tf_grouping_compile.sh
+++ tf_ops/grouping/tf_grouping_compile.sh
@@ -17,9 +17,11 @@
 #g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I$TF_INC -I /usr/local/cuda-10.0/include -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-10.0/lib64/ -L$TF_LIB -ltensorflow_framework -O2 #-D_GLIBCXX_USE_CXX11_ABI=0 
 
 #/bin/bash
-/usr/local/cuda-10.1/bin/nvcc tf_grouping_g.cu -o tf_grouping_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC 
-TF_INC=$(python -c ‘import tensorflow as tf; print(tf.sysconfig.get_include())’)
-TF_LIB=$(python -c ‘import tensorflow as tf; print(tf.sysconfig.get_lib())’)
-CUDA_INC=’/usr/local/cuda-10.1/include’
-CUDA_LIB=’/usr/local/cuda-10.1/lib64/’ 
+/usr/local/cuda-10.1/bin/nvcc tf_grouping_g.cu -o tf_grouping_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+CUDA_INC='/usr/local/cuda-10.1/include'
+CUDA_LIB='/usr/local/cuda-10.1/lib64/'
+
 g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I $TF_INC -I $CUDA_INC -I $TF_INC/external/nsync/public -lcudart -L $CUDA_LIB -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
